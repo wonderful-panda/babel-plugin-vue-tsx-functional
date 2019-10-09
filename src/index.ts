fe5ac38aa0ca58306ae48f3bad259a8ddabe0959
iwata hidetaka <iwata0303@gmail.com>
2019-10-10 06:51:26 +0900

    working on

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
+import pp from "ast-pretty-print";
+import * as babel from "@babel/core";
 import * as t from "@babel/types";
-import { createMacro } from "babel-plugin-macros";
+import { createMacro, MacroError } from "babel-plugin-macros";
 
-const processRef = (_ref: t.Node, _babel: typeof import("@babel/core")) => {};
+const makeError = (msg: string, nodePath: babel.NodePath) => {
+  const p = nodePath.findParent(p => p.isProgram());
+  let locString = "unknown";
+  if (nodePath.node.loc) {
+    const { line, column } = nodePath.node.loc.start;
+    locString = `${line}:${column}`;
+  }
+  return new MacroError(`[vue-functional.macro] (at ${locString}) ${msg}`);
+};
+
+const processRef = (ref: babel.NodePath, _b: typeof babel) => {
+  const parent = ref.parentPath.node;
+  if (!t.isCallExpression(parent)) {
+    throw makeError("macro must be callee of CallExpression", ref);
+  }
+  if (ref.node !== parent.callee) {
+    throw makeError("macro must be callee of CallExpression", ref);
+  }
+};
 
 const functionalComponentMacro = (arg: {
-  references: Record<string, t.Node[]>;
-  babel: typeof import("@babel/core");
+  references: Record<string, babel.NodePath[]>;
+  babel: typeof babel;
 }) => {
-  for (const key in Object.keys(arg.references)) {
+  for (const key in arg.references) {
     for (const ref of arg.references[key]) {
       processRef(ref, arg.babel);
     }
--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,2 +1,6 @@
 declare module "@babel/plugin-syntax-jsx";
-declare module "babel-plugin-macros";
+declare module "babel-plugin-macros" {
+  function createMacro(func: Function): any;
+  const MacroError: ErrorConstructor;
+}
+declare module "ast-pretty-print";
