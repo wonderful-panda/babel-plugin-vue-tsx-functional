d9d5fd510f61423b1e0a6149aa4ef7fc685c6491
iwata hidetaka <iwata0303@gmail.com>
2019-10-10 23:20:49 +0900

    use vue-tsx-support@dev

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Vue, { VNode, RenderContext } from "vue";
-import { TsxComponent } from "vue-tsx-support";
+import { _TsxComponentV3 } from "vue-tsx-support";
 
 export function __VueFC__<P>(
   _func: (ctx: RenderContext<P>) => VNode
-): TsxComponent<Vue, P> {
+): _TsxComponentV3<Vue, {}, P, {}, {}, {}> {
   throw new Error(
     "[babel-plugin-vue-functional] This function must be removed by babel"
   );
